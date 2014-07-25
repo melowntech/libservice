803009464e8669f6aabf52180bc7322b94b3682f
Václav Blažek <vaclav.blazek@citationtech.net>
2014-07-25 14:20:51 +0000

    shared/service: added runElevated
    git-svn-id: https://scm.citationtech.net/trunk/shared/service/service@10579 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- service/persona.hpp
+++ service/persona.hpp
@@ -3,14 +3,55 @@
 
 #include <boost/optional.hpp>
 
+#include "dbglog/dbglog.hpp"
+
 #include "utility/identity.hpp"
+#include "utility/guarded-call.hpp"
 
 namespace service {
 
 struct Persona {
+    /** Persona at the moment service has been started.
+     */
     utility::Identity start;
+
+    /** Persona service is running at.
+     */
     utility::Identity running;
+
 };
+
+/** Run call with elevated rights (process has been started with) and switch
+ *  back to normal rights.
+ */
+template <typename Call>
+auto runElevated(const boost::optional<Persona> &persona, Call call)
+    -> decltype(call());
+
+// implementation
+
+template <typename Call>
+auto runElevated(const boost::optional<Persona> &persona, Call call)
+    -> decltype(call())
+{
+    // if no persona -> run as is
+    if (!persona) { return call(); }
+
+    // elevate rights, run and return rights back again
+    return utility::guardedCall([&persona]()
+    {
+        LOG(info1) << "Switching to persona: <" << persona->start << ">.";
+        setEffectivePersona(persona->start);
+    }, [&call]
+    {
+        call();
+    }, [&persona]()
+    {
+        LOG(info1) << "Switching back to persona: <"
+                   << persona->running << ">.";
+        setEffectivePersona(persona->running);
+    });
+}
 
 } // namespace service
 
