7c9cf64600b4c85b458e1a7c02249f075f3051fd
Vaclav Blazek <vaclav.blazek@citationtech.net>
2020-01-08 17:42:20 +0100

    updated copyright

--- service/customer/program.seznam.cz.cpp
+++ service/customer/program.seznam.cz.cpp
@@ -30,7 +30,7 @@ namespace service {
 // Default copyright.
 std::string Program::copyright() const
 {
-    return R"RAW(Copyright (C) 2011-2016 Melown Technologies SE
+    return R"RAW(Copyright (C) 2011-2020 Melown Technologies SE
 Strakonicka 1199/2d, 150 00 Praha 5, Czech Republic)RAW";
 }
 
